ac899e25009d6c16ce7840239b967395bf62c018
Dieter Brüning <github@media-bruening.de>
2015-12-12 11:25:10 +0100

    Extension theme basic Files

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "templavoila_framework".
+#
+# Auto generated 25-08-2011 21:37
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Bootstrap 4 Theme',
+	'description' => 'Theme to use Twitter Bootstrap 4',
+	'category' => 'templates',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Themes-Team (Kay Strobach, Jo Hasenau, Thomas Deuling)',
+	'author_email' => 'team@typo3-themes.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3'  => '6.2.0-7.6.99',
+			'themes_gridelements' => '0.6.1-0.6.99',
+			'dyncss_scss' => '1.0.0-1.0.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ * Configuration of theme_bootstrap4
+ *
+ */
+//if(!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('themes')) {
+//    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', $_EXTKEY . ': theme');
+//}
