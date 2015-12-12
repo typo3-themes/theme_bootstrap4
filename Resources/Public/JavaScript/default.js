6c0e0e41ea68be87d8c7524b1b996a0f8f1bec62
Dieter Brüning <github@media-bruening.de>
2015-12-12 11:38:43 +0100

    Include: TS: page.setup, page.includes, page.header, page.inlineSettings; default.js

---
+++ Resources/Public/JavaScript/default.js
@@ -0,0 +1,4 @@
+if (typeof console == "object") {
+	console.log('here comes your own js..');
+	console.log('If you need some constants values, take a look into the js var \'themes\': console.log(themes)');
+}
