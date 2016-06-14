cfb1fe1b8154305334c43b1ea7b057e1bc49415e
Thomas Deuling <info@coding.ms>
2016-06-14 14:32:36 +0200

    [TASK] Working on responsive gridelements

--- Resources/Public/JavaScript/default.js
+++ Resources/Public/JavaScript/default.js
@@ -1,4 +1,24 @@
-if (typeof console == "object") {
-	console.log('here comes your own js..');
-	console.log('If you need some constants values, take a look into the js var \'themes\': console.log(themes)');
-}
+// Define theme object, when it's not already available
+var theme = theme || {};
+jQuery(document).ready(function() {
+	// Process menu actions
+	if(typeof(theme.menu) === 'undefined') {
+		theme.menu = {};
+	}
+	// Language menu
+	theme.menu.language = jQuery('#menu-language');
+	if(theme.menu.language.length>0) {
+		// Only in case of a select-box language menu
+		if(theme.menu.language.is('select')) {
+			// ..bind the on-change
+			theme.menu.language.on('change', function () {
+				document.location = $(this).find(':selected').data('link');
+			});
+		}
+	}
+	
+	
+	
+	
+
+});
