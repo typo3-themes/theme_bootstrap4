9f8d9a66011937fd4130a4b7fc92bc1c01ff8e54
Dieter Brüning <info@media-bruening.de>
2016-10-15 14:00:39 +0200

    [BUGFIX]  Carousel smoth fade

--- Resources/Public/JavaScript/initialize.js
+++ Resources/Public/JavaScript/initialize.js
@@ -7,8 +7,21 @@ jQuery(document).ready(function() {
 	theme.menu.main.scrollOffset = 180;
 	theme.initialize();
 
-	// activate dropdown
-	// http://v4-alpha.getbootstrap.com/components/dropdowns/#via-javascript
-	// jQuery('.dropdown-toggle').dropdown();
-	
+	// @ToDo: maybe move to teme.initialize() ?
+	function carouselFade() {
+		jQuery('.carousel-fade').each(function() {
+			var maxHeight = -1;
+			jQuery(this).find('.carousel-item > div').each(function() {
+				if (jQuery(this).height() > maxHeight) {
+					maxHeight = jQuery(this).height();
+				}
+			});
+			jQuery(this).height(maxHeight);
+		});
+	}
+	carouselFade();
+	jQuery(window).resize(function() {
+		carouselFade();
+	});
+
 });
