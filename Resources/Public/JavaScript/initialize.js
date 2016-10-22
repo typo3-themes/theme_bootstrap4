97e1caf371fab193976cbd687501464b187fff77
Dieter Brüning <info@media-bruening.de>
2016-10-22 14:42:58 +0200

    [TASK]  Carousel smoth fade

--- Resources/Public/JavaScript/initialize.js
+++ Resources/Public/JavaScript/initialize.js
@@ -6,22 +6,4 @@ jQuery(document).ready(function() {
 	theme.body.scrollOffset = 180;
 	theme.menu.main.scrollOffset = 180;
 	theme.initialize();
-
-	// @ToDo: maybe move to teme.initialize() ?
-	function carouselFade() {
-		jQuery('.carousel-fade').each(function() {
-			var maxHeight = -1;
-			jQuery(this).find('.carousel-item > div').each(function() {
-				if (jQuery(this).height() > maxHeight) {
-					maxHeight = jQuery(this).height();
-				}
-			});
-			jQuery(this).height(maxHeight);
-		});
-	}
-	carouselFade();
-	jQuery(window).resize(function() {
-		carouselFade();
-	});
-
 });
