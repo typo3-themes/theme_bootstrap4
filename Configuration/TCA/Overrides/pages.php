75fbcbba31848a77264c93aff9b1f676ed2e12df
dbruening <info@media-bruening.de>
2017-11-20 15:54:12 +0100

    [TASK] TCA Overrides for gridelements and Linkhandler configuration for news

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,20 @@
+<?php
+//
+// Linkhandler Configuration
+// TYPO3 8.7
+if((int)TYPO3_version>=8) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'theme_paperclip',
+        'Configuration/PageTS/Linkhandler/tx_news_news_typo3-8.typoscript',
+        'Linkhandler configuration for News (TYPO3 8.7)'
+    );
+}
+// TYPO3 7.6
+else if((int)TYPO3_version>=7 && (int)TYPO3_version<8) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'theme_paperclip',
+        'Configuration/PageTS/Linkhandler/tx_news_news_typo3-7.typoscript',
+        'Linkhandler configuration for News (TYPO3 7.6)'
+    );
+}
+
