638ca7f8f472b9426dd3ed085e465a8e24cafccc
Kay Strobach <github@kay-strobach.de>
2018-02-14 15:04:36 +0000

    Apply fixes from StyleCI

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,7 +2,7 @@
 //
 // Linkhandler Configuration
 // TYPO3 8.7
-if((int)TYPO3_version>=8) {
+if ((int)TYPO3_version>=8) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
         'theme_paperclip',
         'Configuration/PageTS/Linkhandler/tx_news_news_typo3-8.typoscript',
@@ -10,11 +10,10 @@ if((int)TYPO3_version>=8) {
     );
 }
 // TYPO3 7.6
-else if((int)TYPO3_version>=7 && (int)TYPO3_version<8) {
+elseif ((int)TYPO3_version>=7 && (int)TYPO3_version<8) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
         'theme_paperclip',
         'Configuration/PageTS/Linkhandler/tx_news_news_typo3-7.typoscript',
         'Linkhandler configuration for News (TYPO3 7.6)'
     );
 }
-
