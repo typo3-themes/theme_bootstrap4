d565d99da3a6c193a0267347f5cd2b7582a0355e
Thomas <info@coding.ms>
2018-07-12 15:56:31 +0200

    [TASK] Base configuration for default content elements.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,11 @@ if (!defined('TYPO3_MODE')) {
 }
 
 /*
+ * Mark the delivered TypoScript templates as "content rendering template"
+ */
+$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'themes/Configuration/TypoScript/';
+
+/*
  * Add RTE configuration for theme_bootstrap4
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:theme_bootstrap4/Configuration/RteCkeditor/Default.yaml';
