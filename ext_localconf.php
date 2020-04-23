ea55ca3a9dca111b03c0753e91b0400c7ab1852e
Christian Bülter <cbuelter@coding.ms>
2020-04-23 17:43:09 +0200

    [TASK] Disable registering of non-existing TextImagePreviewRenderer

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,7 +16,8 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:theme_bootstrap4
 /*
  * Register for hook to show preview of tt_content element of CType="themebootstrap4_xxx" in page module
  */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['themebootstrap4_text_image'] =
-    \KayStrobach\ThemeBootstrap4\Hooks\PageLayoutView\TextImagePreviewRenderer::class;
+// @todo The TextImagePreviewRenderer does not exist anymore, there's now a TextMediaPreviewRenderer, this should be registered and the function should be tested
+//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['themebootstrap4_text_image'] =
+    //\KayStrobach\ThemeBootstrap4\Hooks\PageLayoutView\TextImagePreviewRenderer::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['themebootstrap4_image'] =
     \KayStrobach\ThemeBootstrap4\Hooks\PageLayoutView\ImagePreviewRenderer::class;
